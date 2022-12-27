ba983862ad3e8a2a246c360a1c5a8bbbc4bc263b
NikaKlokava <veronika.kazakevich1997@gmail.com>
2022-12-27 21:22:10 +0300

    refactor/pl-016: add interactive and js for header section

--- index.js
+++ index.js
@@ -1,8 +1,35 @@
 function handleBurgerClick() {
   var x = document.getElementById("navbar");
+  var y = document.getElementsByClassName("navbar-item");
+  var z = document.getElementsByClassName("line");
+  for (let item of z) {
+    if (item.className === "line") {
+      item.className += " crossLine";
+    } else {
+      item.className = "line";
+    }
+  }
   if (x.className === "navbar") {
     x.className += " burgerView";
+    for (let item of y) {
+      item.onclick = function (event) {
+        if (event.type === "click") {
+          x.className = "navbar";
+        }
+      };
+    }
   } else {
     x.className = "navbar";
   }
 }
+window.onscroll = function () {
+  scrollFunction();
+};
+
+// function scrollFunction() {
+//   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+//     document.getElementById("header-container").style.padding = "12px 109px 12px 111px";
+//   } else {
+//     document.getElementById("header-container").style.padding = "42px 109px 42px 111px";
+//   }
+// }
