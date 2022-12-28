0fd3440463939c04b8343848dd3f54ed7d81fab8
NikaKlokava <veronika.kazakevich1997@gmail.com>
2022-12-28 11:49:39 +0300

    refactor/pl-016: improve interactive in navbar

--- index.js
+++ index.js
@@ -1,35 +1,52 @@
 function handleBurgerClick() {
-  var x = document.getElementById("navbar");
-  var y = document.getElementsByClassName("navbar-item");
-  var z = document.getElementsByClassName("line");
-  for (let item of z) {
-    if (item.className === "line") {
-      item.className += " crossLine";
-    } else {
-      item.className = "line";
-    }
-  }
+  var x = document.getElementById("navbar"); // это мы получили наш navbar
+  var y = document.getElementsByClassName("navbar-item"); // каждый item из navbar, при нажатии должен быть снова закрыт
+  var z = document.getElementsByClassName("line"); // lines в бургер-иконке (4 шт), чтобы сделать крестик
+  var w = document.getElementsByTagName("section"); //  sections для клика по пустому месту (отличному от header)
+
   if (x.className === "navbar") {
-    x.className += " burgerView";
+    // тут делаем navbar
+    x.className += " burgerView"; // добавили класс
     for (let item of y) {
+      // переберем каждый item из navbar
       item.onclick = function (event) {
+        // при нажатии на item navbar должен закрыться
         if (event.type === "click") {
-          x.className = "navbar";
+          x.className = "navbar"; // присваиваем ему снова navbar (то есть закрываем)
+          for (let item of z) {
+            // в то же время надо, чтобы и крестик тоже превратился в lines
+            if ((x.className = "navbar")) {
+              item.className = "line"; // вернули lines
+            }
+          }
         }
       };
     }
   } else {
-    x.className = "navbar";
+    x.className = "navbar"; // а тут если мы будем просто нажимать на бургер-иконку navbar должен скрыться
+  }
+  for (let item of z) {
+    // тут отдельно проходим по lines, чтобы сделать новый класс для них
+    if (item.className === "line") {
+      item.className += " crossLine"; // добавили класс
+    } else {
+      item.className = "line"; // убрали класс если crossLine уже есть
+    }
+  }
+  for (let item of w) {
+    //тут прошлись по всем sections
+    item.onclick = function (event) {
+      if (event.type === "click") {
+        //тут при нажатии на sections navbar снова будет закрыт
+        x.className = "navbar"; // вот navbar
+        for (let item of z) {
+          // тут нам нужно, чтобы крестик снова превратился в полоски в бургер-иконке
+          if ((x.className = "navbar")) {
+            // поэтому если у нас закрыт navbar, то делаем из него lines
+            item.className = "line"; // вот lines
+          }
+        }
+      }
+    };
   }
 }
-window.onscroll = function () {
-  scrollFunction();
-};
-
-// function scrollFunction() {
-//   if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-//     document.getElementById("header-container").style.padding = "12px 109px 12px 111px";
-//   } else {
-//     document.getElementById("header-container").style.padding = "42px 109px 42px 111px";
-//   }
-// }
